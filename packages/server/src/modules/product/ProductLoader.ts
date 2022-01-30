45f54f475150422a0f799d700c81a719b0e2bc53
Akinn Rosa <akinncar@hotmail.com>
2022-01-30 01:01:03 -0300

    feat(server): crud products

---
+++ packages/server/src/modules/product/ProductLoader.ts
@@ -0,0 +1,15 @@
+import { createLoader } from '../../graphql/createLoader';
+
+import { registerLoader } from '../../graphql/loaderRegister';
+
+import ProductModel from './ProductModel';
+
+const { Wrapper: Product, getLoader, clearCache, load, loadAll } = createLoader({
+  model: ProductModel,
+  loaderName: 'ProductLoader',
+});
+
+export { getLoader, clearCache, load, loadAll };
+export default Product;
+
+registerLoader('ProductLoader', getLoader);
